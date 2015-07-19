87f0945495825dde8cd4bc073f24e6a66d9a67a2
skylerto <skylerclayne@gmail.com>
2015-07-18 22:36:00 -0400

    added new ssh public key grabber from github

---
+++ src/main/java/Scraper/SSHGrabber.java
@@ -0,0 +1,44 @@
+package Scraper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by skylerlayne on 2015-07-18.
+ */
+public class SSHGrabber {
+
+    public String username;
+
+    public SSHGrabber(String username){
+        this.username = username;
+    }
+
+    public String grab(){
+        String key = "";
+        try {
+            URL url = new URL("https://github.com/" + this.username + ".keys");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
+                for (String line; (line = reader.readLine()) != null;) {
+                    return line;
+                }
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return key;
+    }
+
+    public static void main(String[] args) {
+        SSHGrabber grabber = new SSHGrabber("skylerto");
+        System.out.print(grabber.grab());
+    }
+
+}
